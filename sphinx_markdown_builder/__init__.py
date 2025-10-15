fd81abb75b835097cedbe92fa9cd83216b51cf4f
Johannes Altmanninger <aclopte@gmail.com>
2025-10-15 14:36:53 +0200

    Declare extension as safe for parallel reading/writing (#38)
    When I run sphinx-build with "-j auto" I get this warning:
    
    	WARNING: the sphinx_markdown_builder extension does not
    	declare if it is safe for parallel reading, assuming it isn't -
    	please ask the extension author to check and make it explicit
    
    Based on https://www.sphinx-doc.org/en/master/extdev/index.html and
    a cursory look at the code, I think we should be parallel-safe:
    concurrent read/write calls on different documents should not conflict,
    and we never write anything to the build environment object ("env").
    
    Let's enable parallel reading/writing explicitly.
    
    If this is wrong, we should disable it explicitly.

--- sphinx_markdown_builder/__init__.py
+++ sphinx_markdown_builder/__init__.py
@@ -3,12 +3,13 @@ A Sphinx extension to add markdown generation support.
 """
 
 from sphinx_markdown_builder.builder import MarkdownBuilder
+from sphinx.util.typing import ExtensionMetadata
 
 __version__ = "0.6.8"
 __docformat__ = "reStructuredText"
 
 
-def setup(app):
+def setup(app) -> ExtensionMetadata:
     app.add_builder(MarkdownBuilder)
     app.add_config_value("markdown_http_base", "", "html", str)
     app.add_config_value("markdown_uri_doc_suffix", ".md", "html", str)
@@ -17,3 +18,9 @@ def setup(app):
     app.add_config_value("markdown_anchor_signatures", False, "html", bool)
     app.add_config_value("markdown_docinfo", False, "html", bool)
     app.add_config_value("markdown_bullet", "*", "html", str)
+
+    return {
+        "version": __version__,
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
+    }
